fc7d214853af15839c8081908ef8615cd6447526
Malin <Malin@DESKTOP-84S310L>
2018-12-21 21:42:01 +0100

    All standard kod

---
+++ DejtingSajt/DejtingSajt/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(DejtingSajt.Startup))]
+namespace DejtingSajt
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
