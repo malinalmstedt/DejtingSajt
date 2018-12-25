47b938502741866c21920e79e4871b838ba7eb25
Malin <Malin@DESKTOP-84S310L>
2018-12-25 09:07:39 +0100

    Ändrat lösenord till Password

--- DejtingSajt/DejtingSajt/Models/User.cs
+++ DejtingSajt/DejtingSajt/Models/User.cs
@@ -13,7 +13,7 @@ namespace DejtingSajt.Models
         public string Firstname { get; set; }
         public string Lastname{ get; set; }
         public string Email { get; set; }
-        public string Lösenord { get; set; }
+        public string PassWord { get; set; }
     }
 }
 
