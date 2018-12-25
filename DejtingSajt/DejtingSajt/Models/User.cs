efde6005e27bb1ed520846f0ed5045bac50749ca
Malin <Malin@DESKTOP-84S310L>
2018-12-25 09:06:02 +0100

    Lagt till lösenord i User.cs

--- DejtingSajt/DejtingSajt/Models/User.cs
+++ DejtingSajt/DejtingSajt/Models/User.cs
@@ -13,5 +13,7 @@ namespace DejtingSajt.Models
         public string Firstname { get; set; }
         public string Lastname{ get; set; }
         public string Email { get; set; }
+        public string Lösenord { get; set; }
     }
 }
+
